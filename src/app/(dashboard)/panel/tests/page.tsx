e4b46d6f951f59d5634d3a0ba72dd486ac1d9999
RobotoSkunk <contact@robotoskunk.com>
2025-10-03 15:25:04 -0600

    Creada estructura de la tabla de DynamicTable

--- src/app/(dashboard)/panel/tests/page.tsx
+++ src/app/(dashboard)/panel/tests/page.tsx
@@ -9,7 +9,33 @@ export default function Page()
 	return (<>
 		<h1>Pruebas</h1>
 		<DynamicTable
-			fields={ [] }
+			fields={ [
+				{
+					name: 'col_1',
+					type: 'text',
+					label: 'Columna 1',
+				},
+				{
+					name: 'col_2',
+					type: 'number',
+					label: 'Columna 2',
+				},
+				{
+					name: 'col_3',
+					type: 'date',
+					label: 'Columna 3',
+				},
+			] }
+			
+			pagesCount={ 1 }
+
+			rows={
+				[... new Array(50)].map(_ => ({
+					col_1: 'texto',
+					col_2: 12345,
+					col_3: new Date(),
+				}))
+			}
 		/>
 	</>);
 }
--- src/components/DynamicTable/index.tsx
+++ src/components/DynamicTable/index.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import Image from 'next/image';
-import { CSSProperties, HTMLInputTypeAttribute } from 'react';
+import { CSSProperties, HTMLInputTypeAttribute, useState } from 'react';
 
 import style from './style.module.css';
 
@@ -21,7 +21,6 @@ interface FieldBase
 	 * Un nombre leíble por el usuario.
 	 */
 	label: string;
-	value?: string;
 	required?: boolean;
 	className?: string;
 	style?: CSSProperties;
@@ -44,7 +43,19 @@ interface TextAreaField extends FieldBase
 
 interface InputField extends FieldBase
 {
-	type: Omit<Omit<HTMLInputTypeAttribute, 'checkbox'>, 'radio'>;
+	type:
+		| 'checkbox'
+		| 'radio'
+		| 'date'
+		| 'datetime-local'
+		| 'email'
+		| 'file'
+		| 'image'
+		| 'number'
+		| 'password'
+		| 'tel'
+		| 'text'
+		| 'time';
 
 	min?: number;
 	max?: number;
@@ -54,40 +65,93 @@ interface InputField extends FieldBase
 export type Field = TextAreaField | InputField;
 // #endregion
 
+type TypeMap<T extends string> =
+	T extends 'textarea'       ? string  :
+	T extends 'checkbox'       ? boolean :
+	T extends 'radio'          ? boolean :
+	T extends 'date'           ? Date    :
+	T extends 'datetime-local' ? Date    :
+	T extends 'email'          ? string  :
+	T extends 'number'         ? number  :
+	T extends 'password'       ? string  :
+	T extends 'tel'            ? string  :
+	T extends 'text'           ? string  :
+	T extends 'time'           ? string  :
+	string;
+
 
 type Props = {
+	/**
+	 * Define la estructura de los campos de la tabla.
+	 */
+	fields: readonly Field[];
+
+	/**
+	 * Establece el número máximo de páginas que se encuentran disponibles; esta propiedad es meramente decorativa.
+	 * Si el número de páginas es menor o igual a 0, las opciones de paginación son omitidas de la tabla.
+	 */
+	pagesCount: number;
+
+	/**
+	 * Las filas a mostrar en la tabla.
+	 */
+	rows: {
+		readonly [ K in Props['fields'][number] as K['name'] ]: TypeMap<K['type']>;
+	}[];
+
+	/**
+	 * El texto personalizado del botón para crear una nueva entrada.
+	 */
+	newEntryLabel?: string;
+
 	/**
 	 * Se ejecuta cuando el usuario da clic en una opción para crear una nueva entrada. Si no se especifica, las
 	 * opciones para crear entradas son omitidas de la interfaz.
 	 * @returns Verdadero si la operación fue exitosa.
 	 */
-	onCreate?: () => boolean | Promise<boolean>;
+	onCreate?: () => Promise<boolean>;
 
 	/**
 	 * Se ejecuta cuando el usuario da clic en una opción para actualizar una entrada. Si no se especifica, las
 	 * opciones para actualizar entradas son omitidas de la interfaz.
 	 * @returns Verdadero si la operación fue exitosa.
 	 */
-	onUpdate?: () => boolean | Promise<boolean>;
+	onUpdate?: () => Promise<boolean>;
 
 	/**
 	 * Se ejecuta cuando el usuario da clic en una opción para eliminar una entrada. Si no se especifica, las
 	 * opciones para eliminar entradas son omitidas de la interfaz.
 	 * @returns Verdadero si la operación fue exitosa.
 	 */
-	onDelete?: () => boolean | Promise<boolean>;
+	onDelete?: () => Promise<boolean>;
 
 	/**
-	 * Define la estructura de las entradas de la tabla.
+	 * Se ejecuta automáticamente cuando el usuario busca cambiar a otra página.
+	 * @returns Verdadero si la operación fue exitosa.
 	 */
-	fields: Field[];
+	onRequestPage?: (page: number) => Promise<boolean>;
 };
+
+// type FieldsToJson<T extends readonly Field[]> = {
+// 	[ K in T[number] as K['name'] ]: TypeMap<K['type']>;
+// };
 
 
 export default function DynamicTable({
-	
+	fields,
+	rows,
+	newEntryLabel,
 }: Props)
 {
+	const [ page, setPage ] = useState(0);
+
+	// if (onRead) {
+	// 	const result = await onRead(0);
+
+	// 	result.rows[0].
+	// }
+
+
 	return (
 		<div className={ style['dynamic-table'] }>
 			<div className={ style['top-part'] }>
@@ -103,6 +167,52 @@ export default function DynamicTable({
 					/>
 					<input type='search' id='search-input'/>
 				</label>
+
+				<button
+
+				>
+					{ newEntryLabel ?? 'Nueva entrada' }
+				</button>
+			</div>
+
+			<div className={ style.table }>
+				<div className={ style.row }>
+					{ fields.map((field, i) =>
+					{
+						if (field.hideOnTable) {
+							return null;
+						}
+
+						return (
+							<div className={ style.cell } key={ i }>
+								{ field.label }
+							</div>
+						);
+					}) }
+
+					<div className={ style.cell } style={{ width: 150 }}>
+						Acciones
+					</div>
+				</div>
+				{ rows.map((row, i) =>
+				(
+					<div className={ style.row } key={ i }>
+						{ Object.keys(row).map((key, i) =>
+						{
+							let value = row[key] as TypeMap<typeof key>;
+
+							if (typeof value === 'object') {
+								value = (value as Date).toLocaleDateString();
+							}
+
+							return (
+								<div className={ style.cell } key={ i }>
+									{ value }
+								</div>
+							);
+						}) }
+					</div>
+				)) }
 			</div>
 		</div>
 	);
